c055cfb676adbf69ea958ddbed45f5bdbe2d3e8b
t-takaaki <hokahoka.oden@gmail.com>
2020-08-27 22:45:03 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react"
+import { render } from 'react-dom'
+import QrReader from 'react-qr-reader'
+
+
+const Root = () => {
+  const [result, setResult] = useState("No Result.")
+  const handleScan = data => {
+    if (data) {
+      setResult(data)
+    }
+  }
+  const handleError = err => console.error(err)
+  return (
+    <div>
+      <QrReader delay={300} onError={handleError} onScan={handleScan} style={{width: "100%"}} showViewFinder={true} />
+      <div id="result">{result}</div>
+    </div>
+  );
+}
+
+const root = document.getElementById("root")
+render(<Root />, root)
